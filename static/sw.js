c6a4c5ed6b86f263300e249fecfa30b2fffb3ee0
rohith14062007 <rohith1406@gmail.com>
2026-09-15 13:55:00 +0530

    Transform web portal into a Progressive Web App (PWA) with manifest.json, sw.js, app icons and 1-click Install App feature

---
+++ static/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'tnstc-bus-app-v1';
+const urlsToCache = [
+  '/',
+  '/static/manifest.json',
+  '/static/icon-192.png',
+  '/static/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache).catch(err => console.log('SW cache error:', err));
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
